4892f2c3b7325dd7cf468a0ce2e230ffc1406216
Simone Fidanza <fsimone2002@proton.me>
2023-06-09 22:36:21 +0200

    feat(plugins): add plugins overrides

---
+++ plugins/overrides/nvterm.lua
@@ -0,0 +1,16 @@
+return {
+  terminals = {
+    shell = "bash",
+    type_opts = {
+      float = {
+        relative = "editor",
+        row = 0.15,
+        col = 0.10,
+        width = 0.80,
+        height = 0.60,
+        border = require("custom.assets.border"),
+      },
+    },
+  },
+}
+
---
+++ plugins/overrides/which-key.lua
@@ -0,0 +1,9 @@
+return {
+  window = { border = require("custom.assets.border") },
+  layout = {
+    height = { min = 4, max = 25 },
+    width = { min = 20, max = 50 },
+    spacing = 3,
+    align = "center",
+  },
+}
