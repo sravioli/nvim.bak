39b6b950f6d6876ce7a17ef96ede34cc3986a8e3
Simone Fidanza <fsimone2002@proton.me>
2023-06-11 17:06:15 +0200

    feat(overrides): add nvim-tree and mason configs

--- plugins/overrides/mason.lua
+++ plugins/overrides/mason.lua
@@ -2,5 +2,27 @@ return {
   ui = { border = require "custom.assets.border" },
   ensure_installed = {
     "lua-language-server",
+    "stylua",
+
+    "clangd",
+    "clang-format",
+    "cpplint",
+
+    "marksman",
+    "markdownlint",
+    "cbfmt",
+
+    "taplo",
+
+    "yaml-language-server",
+    "yamlfix",
+    "yamllint",
+    "actionlint",
+
+    "json-lsp",
+    "fixjson",
+
+    "cspell",
+    "codespell",
   },
 }
--- plugins/overrides/nvim-tree.lua
+++ plugins/overrides/nvim-tree.lua
@@ -1,1 +1,32 @@
-return {}
+return {
+  view = {
+    mappings = {
+      custom_only = false,
+      list = {},
+    },
+    float = {
+      enable = true,
+      quit_on_focus_loss = true,
+      open_win_config = { width = 35 },
+    },
+  },
+
+  renderer = {
+    indent_markers = { enable = true },
+    root_folder_label = ":~:s?$?/..?",
+    highlight_opened_files = "icon",
+    icons = {
+      git_placement = "after",
+      show = { git = true },
+    },
+  },
+
+  git = { enable = true },
+  trash = { cmd = "rip" },
+
+  actions = {
+    file_popup = {
+      open_win_config = { border = require "custom.assets.border" },
+    },
+  },
+}
