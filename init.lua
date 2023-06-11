6487dda193ca6155d1407338c03b6cd5fc6f2bf2
Simone Fidanza <fsimone2002@proton.me>
2023-06-11 17:08:18 +0200

    feat: add table align function

--- init.lua
+++ init.lua
@@ -1,5 +1,4 @@
 -- autocmds, nvim settings and more here
-local __border = require "custom.assets.border"
 
 -- mimic vimscript syntax
 local let = vim.g
@@ -15,10 +14,12 @@ set.updatetime = 200
 set.tabstop = 2
 set.shiftwidth = 2
 set.expandtab = true
+set.termguicolors = true
 
-let.code_action_menu_window_border = __border
+let.code_action_menu_window_border = require "custom.assets.border"
 let.load_doxygen_syntax = 1
 let.doxygen_javadoc_autobrief = 0
+let.lua_snippets_path = vim.fn.stdpath "config" .. "/lua/custom/snippets"
 
 -- load the desired providers
 local providers = {
@@ -32,7 +33,24 @@ for _, provider in ipairs(providers) do
   let[provider.prov .. "_host_prog"] = provider.path
 end
 
-let.lua_snippets_path = vim.fn.stdpath "config" .. "/lua/custom/snippets"
+function _G.align_markdown_table()
+  local pattern = "^%s*|%s.*%s|%s*$"
+  local linenr, colnr = vim.fn.line ".", vim.fn.col "."
+  local curr_line = vim.fn.getline "."
+  local prev_line, next_line = vim.fn.getline(linenr - 1), vim.fn.getline(linenr + 1)
+
+  if
+    vim.fn.exists ":Tabularize"
+    and curr_line:match "^%s*|"
+    and (prev_line:match(pattern) or next_line:match(pattern))
+  then
+    local col = #curr_line:sub(1, colnr):gsub("[^|]", "")
+    local pos = #vim.fn.matchstr(curr_line:sub(1, colnr), ".*|\\s*\\zs.*")
+    vim.cmd "Tabularize/|/l1" -- `l` means left aligned and `1` means one space of cell padding
+    vim.cmd "normal! 0"
+    vim.fn.search(("[^|]*|"):rep(col) .. ("\\s\\{-\\}"):rep(pos), "ce", linenr)
+  end
+end
 
 -- load custom autocommands
 require "custom.autocommands"
