24b33abf62b98dcefef8bfe9680b3cc97d93f607
Simone Fidanza <fsimone2002@proton.me>
2023-06-13 17:22:09 +0200

    fix(texlab): use latexmk, dump artifacts in build directory

--- plugins/configs/lsp/texlab.lua
+++ plugins/configs/lsp/texlab.lua
@@ -1,21 +1,35 @@
 return {
-  auxDirectory = ".",
-  bibtexFormatter = "texlab",
+  auxDirectory = "build",
+
   build = {
-    args = { "-pdf", "-interaction=nonstopmode", "-synctex=1", "%f" },
-    executable = "pdflatex",
-    forwardSearchAfter = false,
+    executable = "latexmk",
+    args = { "-pdf", "-interaction=nonstopmode", "-synctex=1", "-outdir=build", "%f" },
+    forwardSearchAfter = true,
     onSave = true,
   },
+
+  forwardSearch = {
+    executable = "sioyek.exe",
+    args = {
+      "--reuse-window",
+      "--execute-command",
+      "toggle_synctex", -- Open Sioyek in synctex mode.
+      "--inverse-search",
+      [[nvim --headless -c "Lazy load nvim-lspconfig" %f -c "%l" --server ]] .. vim.v.servername,
+      "--forward-search-file",
+      "%f",
+      "--forward-search-line",
+      "%l",
+      "%p",
+    },
+  },
+  bibtexFormatter = "texlab",
   chktex = {
     onEdit = false,
     onOpenAndSave = true,
   },
   diagnosticsDelay = 300,
   formatterLineLength = 80,
-  forwardSearch = {
-    args = {},
-  },
   latexFormatter = "latexindent",
   latexindent = {
     modifyLineBreaks = true,
